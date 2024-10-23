da0752e270f679cd91842bd522e4347c895f957b
farah-naz123 <fnaz29103@gmail.com>
2024-10-23 20:34:38 +0500

    Add files via upload

--- page.tsx
+++ page.tsx
@@ -1,15 +1,11 @@
-import Navbar from "./components/navbar"
-import Hero from "./components/hero";
-import Footer from "./components/footer";
+import React from 'react'
 
-const HomePage=() => {
+const ContactPage = () => {
   return (
     <div>
-      <Navbar></Navbar>
-      <Hero></Hero>
-      <Footer></Footer>
+      <h1 className='bg-yellow-300'>This is contact page</h1>
     </div>
   )
+}
 
-};
-export default HomePage;
+export default ContactPage
